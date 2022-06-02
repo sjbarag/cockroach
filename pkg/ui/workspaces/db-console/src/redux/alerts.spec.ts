478797e6f4a53cab53f9f7f8c5b4bf95035728d0
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-06-02 16:11:54 -0400

    ui: clearing session storage for Jest test
    When running `src/redux/alerts.spec.ts` in Jest, Sinon and JSDom's
    sessionStorage don't seem to be working as expected so session
    information was persisted across tests (causing failures). I added
    `sessionStorage.clear()` to `afterEach` to prevent this and dropped
    Sinon.
    
    Release note: None

--- pkg/ui/workspaces/db-console/src/redux/alerts.spec.ts
+++ pkg/ui/workspaces/db-console/src/redux/alerts.spec.ts
@@ -11,7 +11,6 @@
 import { assert } from "chai";
 import { Store } from "redux";
 import moment from "moment";
-import sinon from "sinon";
 import { createHashHistory } from "history";
 
 import * as protos from "src/js/protos";
@@ -48,8 +47,6 @@ import {
 } from "./apiReducers";
 import MembershipStatus = cockroach.kv.kvserver.liveness.livenesspb.MembershipStatus;
 
-const sandbox = sinon.createSandbox();
-
 describe("alerts", function() {
   let store: Store<AdminUIState>;
   let dispatch: typeof store.dispatch;
@@ -59,14 +56,11 @@ describe("alerts", function() {
     store = createAdminUIStore(createHashHistory());
     dispatch = store.dispatch;
     state = store.getState;
-    // localSettings persist values in sessionStorage and
-    // this stub disables caching values between tests.
-    sandbox.stub(sessionStorage, "getItem").returns(null);
   });
 
   afterEach(function() {
-    sandbox.restore();
     fetchMock.restore();
+    sessionStorage.clear();
   });
 
   describe("selectors", function() {
