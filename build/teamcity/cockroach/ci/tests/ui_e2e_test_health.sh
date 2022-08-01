b449e6141bb983e8a9def2ce4268cb36c5232bf2
Sean Barag <barag@cockroachlabs.com>
2022-08-01 16:21:24 +0000

    fixup! ui: reintroduce end-to-end UI tests with cypress
    fix imports in teamcity scripts

--- build/teamcity/cockroach/ci/tests/ui_e2e_test_health.sh
+++ build/teamcity/cockroach/ci/tests/ui_e2e_test_health.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source "$(dirname "${0}")/teamcity-support.sh"
-source "./ui_e2e_test_impl.sh"
+dir="$(dirname $(dirname $(dirname $(dirname $(dirname "${0}")))))"
+source "$dir/teamcity-support.sh" # for tc_prepare, tc_start_block, and friends
+source "./ui_e2e_test_impl.sh" # for build_docker_image and run_tests
 
 tc_prepare
 
